3e47546d16f82e486f91d82440d5e9e73351498d
Dhruti Parekh <dhruti.parekh266@gmail.com>
2017-12-10 16:21:39 -0500

    Home and book itinerary activities created

--- app/src/main/java/com/gitlab/airlineticketbooking/HomeActivity.java
+++ app/src/main/java/com/gitlab/airlineticketbooking/HomeActivity.java
@@ -1,13 +1,184 @@
 package com.gitlab.airlineticketbooking;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import com.gitlab.airlineticketbooking.data.ATBContract;
+import com.gitlab.airlineticketbooking.data.ATBContract.Flight;
+import com.gitlab.airlineticketbooking.data.ATBContract.User;
+import com.gitlab.airlineticketbooking.data.ATBDbHelper;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class HomeActivity extends Activity {
+
+    private TextView txtWelcome;
+    private EditText origin;
+    private EditText destination;
+    private EditText departureDate;
+    private Button btnEditProfile;
+    private Button btnViewFlights;
+    private Button btnSearch;
+    private ListView flightList;
+    private ArrayList<String> arrayList;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<Integer> idList;
+    String email;
+    int userId;
+
+    Calendar myCal;
+    DatePickerDialog.OnDateSetListener date;
+    ATBDbHelper myHelper;
+    SQLiteDatabase myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        txtWelcome = (TextView) findViewById(R.id.txtWelcome);
+        origin = (EditText) findViewById(R.id.editOrigin);
+        destination = (EditText) findViewById(R.id.editDestination);
+        departureDate = (EditText) findViewById(R.id.editDepartureDate);
+        btnEditProfile = (Button) findViewById(R.id.btnEditProfile);
+        btnViewFlights = (Button) findViewById(R.id.btnViewFlights);
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+        flightList = (ListView) findViewById(R.id.flightList);
+        arrayList = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arrayList);
+
+        idList = new ArrayList<Integer>(); // stores each flight id of displayed flight in the list view
+
+        //get user first and last name
+        myHelper = new ATBDbHelper(HomeActivity.this);
+        myDb = myHelper.getWritableDatabase();
+
+        Intent i = getIntent();
+        email = i.getStringExtra("email");
+        Cursor userInfo = null;
+        userInfo = myDb.rawQuery("SELECT * FROM " + User.TABLE_NAME
+                                + " WHERE " + User.COLUMN_NAME_EMAIL + "='" + email + "'", null);
+
+        if(userInfo.getCount() != 0){
+            userInfo.moveToNext();
+            userId = userInfo.getInt(0);
+            txtWelcome.setText("Welcome " + userInfo.getString(1) + " " + userInfo.getString(2));
+        }
+
+        // date picker
+        myCal = Calendar.getInstance();
+        date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCal.set(Calendar.YEAR, year);
+                myCal.set(Calendar.MONTH, monthOfYear);
+                myCal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                setDate();
+            }
+
+        };
+
+        // when user clicks on departure date edit text date picker dialog box will be opened
+        departureDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(HomeActivity.this, date, myCal
+                        .get(Calendar.YEAR), myCal.get(Calendar.MONTH),
+                        myCal.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                boolean flag = false;
+                // validates the user data
+                if(origin.length()==0 ) {
+                    origin.setError("Please enter valid origin of your trip");
+                }
+                else if(destination.length()==0 ) {
+                    destination.setError("Please enter valid destination of your trip");
+                }
+                else if(departureDate.length()==0) {
+                    departureDate.setError("Please enter valid departure date");
+                }
+                else
+                {
+                    flag = true;
+                }
+
+                if (flag){
+                    Cursor cursor = null ;
+                    // gets the flight details according to origin, destination and departure date entered by user
+                    cursor = myDb.rawQuery("SELECT * FROM " + Flight.TABLE_NAME
+                            + " WHERE " + Flight.COLUMN_NAME_ORIGIN + "='" + origin.getText() + "' AND "
+                            + Flight.COLUMN_NAME_DESTINATION + "='" + destination.getText() + "' AND "
+                            + Flight.COLUMN_NAME_DEPARTURE_DATE + "='" + departureDate.getText() + "' ORDER BY " + Flight.COLUMN_NAME_TOTAL_COST, null);
+
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    flightList.setAdapter(adapter);
+                    adapter.clear();
+
+                    if(cursor.getCount() != 0){
+                        while (cursor.moveToNext()) {
+                            // adds data to listview
+                            idList.add(cursor.getInt(0));
+                            arrayList.add(cursor.getString(1) + " - Total Cost : $" + cursor.getString(7));
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                    else
+                    {
+                        Toast.makeText(HomeActivity.this, "No results for given data", Toast.LENGTH_LONG).show();
+
+                    }
+
+                    // when user selects itinerary from the list, user will be redirected to BookItinerary activity
+                    flightList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+                    {
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long i)
+                        {
+                            Intent intent = new Intent(HomeActivity.this, BookItineraryActivity.class);
+                            intent.putExtra("flightId", idList.get(position));
+                            intent.putExtra("userId", userId);
+                            startActivity(intent);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    // adds the selected date from date picker to departure date editText
+    private void setDate() {
+        String myFormat = "dd/MM/yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        departureDate.setText(sdf.format(myCal.getTime()));
+
     }
 }
--- app/src/main/java/com/gitlab/airlineticketbooking/data/ATBDbHelper.java
+++ app/src/main/java/com/gitlab/airlineticketbooking/data/ATBDbHelper.java
@@ -6,11 +6,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import com.gitlab.airlineticketbooking.data.ATBContract.User;
 import com.gitlab.airlineticketbooking.data.ATBContract.Flight;
+import com.gitlab.airlineticketbooking.data.ATBContract.BookingInfo;
 
 public class ATBDbHelper extends SQLiteOpenHelper{
 
     private static final String DATABASE_NAME = "atb.db";
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 4;
 
     public ATBDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -29,15 +30,29 @@ public class ATBDbHelper extends SQLiteOpenHelper{
                 + User.COLUMN_NAME_GENDER + " INTEGER NOT NULL);";
         db.execSQL(SQL_CREATE_USER_TABLE);
 
-        String SQL_CREATE_FLIGHT_TABLE = "CREATE TABLE " + Flight.TABLE_NAME + "("
-                + Flight._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + Flight.COLUMN_NAME_USER_ID + " TEXT NOT NULL, "
-                + Flight.COLUMN_NAME_FLIGHT_INDEX + " TEXT NOT NULL); ";
-        db.execSQL(SQL_CREATE_FLIGHT_TABLE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+        // create statement for flight table
+        String SQL_CREATE_FLIGHT_TABLE = "CREATE TABLE " + Flight.TABLE_NAME + "("
+                + Flight._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + Flight.COLUMN_NAME_FLIGHT_NAME + " TEXT NOT NULL, "
+                + Flight.COLUMN_NAME_FLIGHT_NUMBER + " TEXT NOT NULL, "
+                + Flight.COLUMN_NAME_ORIGIN + " TEXT NOT NULL, "
+                + Flight.COLUMN_NAME_DESTINATION + " TEXT NOT NULL, "
+                + Flight.COLUMN_NAME_DEPARTURE_DATE + " TEXT NOT NULL, "
+                + Flight.COLUMN_NAME_ARRIVAL_DATE + " TEXT NOT NULL, "
+                + Flight.COLUMN_NAME_TOTAL_COST + " REAL NOT NULL, "
+                + Flight.COLUMN_NAME_TOTAL_TIME + " TEXT NOT NULL); ";
+
+        // create statement for Booking info table
+        String SQL_CREATE_BOOKING_INFO_TABLE = "CREATE TABLE " + BookingInfo.TABLE_NAME + "("
+                + BookingInfo._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + BookingInfo.COLUMN_NAME_USER_ID + " INTEGER NOT NULL, "
+                + BookingInfo.COLUMN_NAME_FLIGHT_ID + " INTEGER NOT NULL); ";
+        db.execSQL(SQL_CREATE_BOOKING_INFO_TABLE);
 
     }
 }
